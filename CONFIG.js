77174424c0577dd70e86cee45f654036f9b94cf7
王凯 <1602219062@qq.com>
2022-04-27 16:12:06 +0800

    'hhhh'

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "箫洒♡情人节快乐",
+  descriptionHeader:
+    "做我女朋友好吗，如果你出去不回答，那就意味着答应了。^^",
+  buttonYes: "同意",
+  buttonNo: "不同意",
+  titleModar: "我的意中人",
+  descriptionModar:
+    '我就知道你会同意做我的爱人 ^^ </br> 谢谢你同意让我保护你 </br> 对我来说，你会永远是一个小女孩 </br>  未来请多多指教 ^^',
+};
